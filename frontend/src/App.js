36d9cda2661d813dbe89655d4531585830196392
epicharshgupta <harshitguptapranjul@gmail.com>
2026-02-27 22:44:04 +0530

    Frontend routing and pages setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AdminDashboard from "./pages/AdminDashboard";
+import VendorDashboard from "./pages/VendorDashboard";
+import UserDashboard from "./pages/UserDashboard";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import GuestList from "./pages/GuestList";
+import OrderStatus from "./pages/OrderStatus";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Routes>
+
+        <Route path="/" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/admin" element={<AdminDashboard />} />
+
+        <Route path="/vendor" element={<VendorDashboard />} />
+
+        <Route path="/user" element={<UserDashboard />} />
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/checkout" element={<Checkout />} />
+
+        <Route path="/guests" element={<GuestList />} />
+
+        <Route path="/orders" element={<OrderStatus />} />
+
+      </Routes>
+
+    </Router>
   );
+
 }
 
 export default App;
