b901a2cd9dff3b7b476f1bb78be630c8910f30cb
epicharshgupta <harshitguptapranjul@gmail.com>
2026-02-27 21:17:38 +0530

    MongoDB Atlas connected successfully

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -11,6 +15,8 @@ app.get("/", (req, res) => {
   res.send("Event Management API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
